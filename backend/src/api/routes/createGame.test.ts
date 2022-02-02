1599efceebb16073dc8084f7c033ad90ef830be0
Rickard Lindahl <rickard.lindahl@sartorius.com>
2022-02-02 20:07:11 +0100

    Update createGame route tests

--- backend/src/api/routes/createGame.test.ts
+++ backend/src/api/routes/createGame.test.ts
@@ -20,104 +20,106 @@ describe("Create Game endpoint", () => {
     await server.stop();
   });
 
-  describe(`${HttpMethod.Post} method`, () => {
-    describe("Without a payload", () => {
-      it("responds with 400", async () => {
-        const res = await server.inject({
-          method: HttpMethod.Post,
-          url: createGamePath,
-          payload: undefined,
-        });
-        expect(res.result).to.equal({
-          statusCode: 400,
-          error: "Bad Request",
-          message: "Invalid request payload input",
-        });
+  describe("POSTing without a payload", () => {
+    it("should respond with 400", async () => {
+      const res = await server.inject({
+        method: HttpMethod.Post,
+        url: createGamePath,
+        payload: undefined,
+      });
+
+      expect(res.result).to.equal({
+        statusCode: 400,
+        error: "Bad Request",
+        message: "Invalid request payload input",
       });
     });
+  });
 
-    describe("Non-JSON payload", () => {
-      it("responds with 400", async () => {
-        const res = await server.inject({
-          method: HttpMethod.Post,
-          url: createGamePath,
-          payload: "Not JSON",
-        });
-        expect(res.result).to.equal({
-          statusCode: 400,
-          error: "Bad Request",
-          message: "Invalid request payload JSON format",
-        });
+  describe("POSTing with a non-JSON payload", () => {
+    it("should respond with 400", async () => {
+      const res = await server.inject({
+        method: HttpMethod.Post,
+        url: createGamePath,
+        payload: "Not JSON",
+      });
+
+      expect(res.result).to.equal({
+        statusCode: 400,
+        error: "Bad Request",
+        message: "Invalid request payload JSON format",
       });
     });
+  });
 
-    describe("Valid JSON without a name property", () => {
-      it("responds with 400", async () => {
-        const res = await server.inject({
-          method: HttpMethod.Post,
-          url: createGamePath,
-          payload: {
-            foo: "bar",
-          },
-        });
-        expect(res.result).to.equal({
-          statusCode: 400,
-          error: "Bad Request",
-          message: "Invalid request payload input",
-        });
+  describe("POSTing with valid JSON without a name property", () => {
+    it("should respond with 400", async () => {
+      const res = await server.inject({
+        method: HttpMethod.Post,
+        url: createGamePath,
+        payload: {
+          foo: "bar",
+        },
+      });
+
+      expect(res.result).to.equal({
+        statusCode: 400,
+        error: "Bad Request",
+        message: "Invalid request payload input",
       });
     });
+  });
 
-    describe("Valid JSON with an empty name", () => {
-      it("responds with 400", async () => {
-        const res = await server.inject({
-          method: HttpMethod.Post,
-          url: createGamePath,
-          payload: {
-            name: "",
-          },
-        });
-        expect(res.result).to.equal({
-          statusCode: 400,
-          error: "Bad Request",
-          message: "Invalid request payload input",
-        });
+  describe("Valid JSON with an empty name", () => {
+    it("should respond with 400", async () => {
+      const res = await server.inject({
+        method: HttpMethod.Post,
+        url: createGamePath,
+        payload: {
+          name: "",
+        },
+      });
+
+      expect(res.result).to.equal({
+        statusCode: 400,
+        error: "Bad Request",
+        message: "Invalid request payload input",
       });
     });
+  });
 
-    describe("Valid JSON with too long name", () => {
-      it("responds with 400", async () => {
-        const res = await server.inject({
-          method: HttpMethod.Post,
-          url: createGamePath,
-          payload: {
-            name: new Array(512).join("a"),
-          },
-        });
+  describe("Valid JSON with too long name", () => {
+    it("should respond with 400", async () => {
+      const res = await server.inject({
+        method: HttpMethod.Post,
+        url: createGamePath,
+        payload: {
+          name: new Array(512).join("a"),
+        },
+      });
 
-        expect(res.result).to.equal({
-          statusCode: 400,
-          error: "Bad Request",
-          message: "Invalid request payload input",
-        });
+      expect(res.result).to.equal({
+        statusCode: 400,
+        error: "Bad Request",
+        message: "Invalid request payload input",
       });
     });
+  });
 
-    describe("Valid payload", () => {
-      it("responds with 200 and a game id", async () => {
-        const res = await server.inject({
-          method: HttpMethod.Post,
-          url: createGamePath,
-          payload: {
-            name: "Rickard",
-          },
-        });
-
-        expect(res.statusCode).to.equal(200);
-        expect(res.payload).to.match(
-          /^{"id":"[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}"}/,
-        );
+  describe("Valid payload", () => {
+    it("should respond with 200 and a game id", async () => {
+      const res = await server.inject({
+        method: HttpMethod.Post,
+        url: createGamePath,
+        payload: {
+          name: "Pelle",
+        },
       });
+
+      expect(res.statusCode).to.equal(200);
+      expect(res.payload).to.match(
+        /^{"id":"[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}"}/,
+      );
     });
   });
 });
